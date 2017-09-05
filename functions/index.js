2d0c746da3ec8e185ca64200acf42ae7a9b1f19e
Yastremsky Vitaly <yastremsky@gambit.ru>
2017-09-05 16:43:30 +0300

    remove comments

--- functions/index.js
+++ functions/index.js
@@ -14,27 +14,10 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     let base = admin.database().ref('/sub_categories');
     let list = admin.database().ref('/list');
     
-    // base.orderByChild('title').on('value', (data) => {
-    //     console.log(typeof data.val(), data.val(), data.val()[0]);
-    // })
 
-//     base.orderByChild('title').equalTo(request.query.text).on('value', (snapshot) => {
-//         console.log(snapshot.val());
-
-//     },
-//     (err) => console.log(err, "ERRROE"))
-
-
-//     response.send("STRING");
-
-// });
-let sur = ' '
 list.orderByChild('tags').on('value', (snapshot) => {
-    let arr = [];
-        console.log(snapshot)
+        let sur = ' '
         snapshot.forEach((item) => {
-            // console.log(item.val().tags)
-            // console.log( Array.isArray(item.val().tags))
             if(typeof item.val().tags !== "undefined") {
                 if(request.query.text.indexOf(',') === -1) {
                     if(item.val().tags.indexOf(request.query.text) !== -1) {
@@ -42,59 +25,16 @@ list.orderByChild('tags').on('value', (snapshot) => {
                         sur += item.key + ", "
                     }
                 } else {
-                    let spli = request.query.text.split(', ');
-                    console.log(spli, item.val().tags)
-                    
+                    let spli = request.query.text.split(', '); 
                 }
                 
             }
         })
-        // console.log("SNP", snapshot.val());
-        // console.log(arr)
-        // return arr;
-        response.send(sur)
-       return arr;
+
+       response.send(sur)
+       return sur;
     },
     (err) => console.log(err, "ERRROE"))
 
-    // promises().then(res => console.log(res)).catch(err => console.log(err))
-    
-
 });
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-// exports.addMessage = functions.https.onRequest((req, res) => {
-//     // Grab the text parameter.
-//     const original = req.query.text;
-//     // Push the new message into the Realtime Database using the Firebase Admin SDK.
-//     admin.database().ref('/sub_categories').push({title: title}).then(snapshot => {
-//       // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-//       res.redirect(303, snapshot.ref);
-//     });
-//   });
 
-// exports.makeUppercase = functions.database.ref('/messages/{pushId}/original')
-//   .onWrite(event => {
-//     // Grab the current value of what was written to the Realtime Database.
-//     const original = event.data.val();
-//     console.log('Uppercasing', event.params.pushId, original);
-//     const uppercase = original.toUpperCase();
-//     // You must return a Promise when performing asynchronous tasks inside a Functions such as
-//     // writing to the Firebase Realtime Database.
-//     // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-//     return event.data.ref.parent.child('uppercase').set(uppercase);
-//   });
-
-exports.filterTag = functions.database.ref('/list')
-  .onWrite(event => {
-    // Grab the current value of what was written to the Realtime Database.
-    const collectionRef = event.data.ref.parent;
-    const original = event.data.val();
-
-    console.log('S', original, event);
-    // const uppercase = original.toUpperCase();
-    // You must return a Promise when performing asynchronous tasks inside a Functions such as
-    // writing to the Firebase Realtime Database.
-    // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-    return null;
-  });
