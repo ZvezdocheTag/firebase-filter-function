781224145fb423c401152ef269d09bbede2fb870
Yastremsky Vitaly <yastremsky@gambit.ru>
2017-09-07 20:15:11 +0300

    send str if not find

--- functions/index.js
+++ functions/index.js
@@ -9,30 +9,39 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
     const original = request.query.text;
     let base = admin.database().ref('/sub_categories');
     let list = admin.database().ref('/list');
-    
+      
 
 list.orderByChild('tags').on('value', (snapshot) => {
-        let sur = ' '
+         let sur = ''; 
         snapshot.forEach((item) => {
             if(typeof item.val().tags !== "undefined") {
                 if(request.query.text.indexOf(',') === -1) {
-                    console.log(item.val())
+                    console.log(item.val().tags.indexOf(request.query.text) )
                     if(item.val().tags.indexOf(request.query.text) !== -1) {
-                        sur += JSON.stringify(item.val()) + ','
-                    }
+                        // sur += JSON.stringify(item.val()) + ','
+              
+                        sur += item.key + ','
+                    } 
+
                 } else {
                     let spli = request.query.text.split(','); 
                     spli.forEach(subitem => {
                         if(item.val().tags.indexOf(subitem) !== -1) {
-                            sur += JSON.stringify(item.val()) + ','
-                        } 
+                            // sur += JSON.stringify(item.val()) + ','
+                            sur += item.key + ','
+                        }
                     })
                 }
                 
             }
         })
+        if(sur.length === 0) {
+            sur = "null"
+        } 
 
-       response.send(sur)
+        response.send(sur)
+        
+     
        return sur;
     },
     (err) => console.log(err, "ERRROE"))
